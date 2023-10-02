d46088e69d3fe36789d4224d5946198e302ae742
ectionboy <125730248+ectionboy@users.noreply.github.com>
2023-10-02 18:14:56 +0300

    Without search 2

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,6 +1,6 @@
-import { MoviesList } from 'components/MoviesList/MoviesList';
+//import { MoviesList } from 'components/MoviesList/MoviesList';
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 const Movies = () => {
   return (
